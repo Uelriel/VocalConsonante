e4a56347fd65b3de2f3ce52d29629c4c823c7133
David220220 <davidrh99@hotmail.com>
2024-11-11 08:48:26 -0600

    Actualizar VocalConsonante.java
    agregué detectar mayúsculas

--- VocalConsonante.java
+++ VocalConsonante.java
@@ -2,7 +2,7 @@ public class VocalConsonante {
     public static void main(String[] args) {
         char ch = args[0].charAt(0);
 
-        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
             System.out.println(ch + " es una vocal.");
         else 
             System.out.println(ch + " es una consonante.");
