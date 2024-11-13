760d5ac6b5a1f78e68b05aa825aabc4181cb908e
Uelriel <urielleiruuriel@gmail.com>
2024-11-12 21:43:23 -0600

    Add files via upload

--- VocalConsonante.java
+++ VocalConsonante.java
@@ -1,10 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package vocalconsonante;
+
+/**
+ *
+ * 
+ */
+import java.util.Scanner;
+
 public class VocalConsonante {
     public static void main(String[] args) {
-        char ch = args[0].charAt(0);
+        Scanner scanner = new Scanner(System.in);
+        char ch;
 
-        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
-            System.out.println(ch + " es una vocal.");
-        else 
-            System.out.println(ch + " es una consonante.");
+        System.out.println("Ingresa una letra (presione 'q' para salir): ");
+        
+        while (true) {
+            System.out.print("Ingresa una letra: ");
+            ch = scanner.next().toLowerCase().charAt(0); 
+            
+     
+            if (ch == 'q') {
+                System.out.println("Programa terminado.");
+                break;
+            }
+
+
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                System.out.println(ch + " es una vocal.");
+            } else { 
+                
+                System.out.println(ch + " es una consonante.");
+            }
+        }
     }
 }
